d32a9fe96c59acda076048937660132678731055
thequanic <85480632+thequanic@users.noreply.github.com>
2024-10-04 19:16:20 +0530

    spelling mistake corrected

--- app1/src/Components/NavBar.js
+++ app1/src/Components/NavBar.js
@@ -39,7 +39,7 @@ export default function NavBar()
         
         <nav className="navbar navbar-expand-lg  navbar-dark " style= {str}>
     <div className="container-fluid">
-    <Link className="navbar-brand" to="/">My Dairy</Link>
+    <Link className="navbar-brand" to="/">My Diary</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={handleNavCollapse}>
       <span className="navbar-toggler-icon"></span>
     </button>
